2e9217d643ab2437d4ea816e037d1b9bc94b0151
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-09-29 11:13:38 -0700

    prototype gui with search filter

---
+++ src/foo.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import SearchInput, {createFilter} from 'react-search-input'
+import './App.css';
+
+
+const repairs = [
+{"id": 0, "repair": "iPhone 3G Loud Speaker Repair", "name": "iphone 3g", "price": "$39.99", "date": "Thu Sep 28 17:43:30 2017", "url": "https://www.ubreakifix.com/iphone-repair/iphone-3g-repair/iphone-3g-loud-speaker-repair"},
+{"id": 1, "repair": "iPhone 3G LCD Replacement", "name": "iphone 3g", "price": "$39.99", "date": "Thu Sep 28 17:43:30 2017", "url": "https://www.ubreakifix.com/iphone-repair/iphone-3g-repair/iphone-3g-lcd-replacement"},
+{"id": 2, "repair": "iPhone 3G Home Button Replacement", "name": "iphone 3g", "price": "$39.99", "date": "Thu Sep 28 17:43:30 2017", "url": "https://www.ubreakifix.com/iphone-repair/iphone-3g-repair/iphone-3g-home-button-replacement"},
+{"id": 3, "repair": "iPhone 3G Headphone Jack Repair", "name": "iphone 3g", "price": "$39.99", "date": "Thu Sep 28 17:43:30 2017", "url": "https://www.ubreakifix.com/iphone-repair/iphone-3g-repair/iphone-3g-headphone-jack-repair"},
+]
+
+const KEYS_TO_FILTERS = ['repair']
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      repairs: repairs,
+      searchTerm: '',
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
+
+  render() {
+    const filteredRepairs = repairs.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+    return (
+      <div>
+        <SearchInput className="search-input" onChange={this.searchUpdated} />
+        {filteredRepairs.map(repair => {
+          return (
+            <div className="id" key={repair.id}>
+              <div className="name">{repair.name}</div>
+              <div className="repair">{repair.repair}</div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default App;
